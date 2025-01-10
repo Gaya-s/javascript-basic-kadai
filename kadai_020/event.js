370d1f1fe88f6da98b985f95b382642bf6e53050
Gaya-s <zru145.16@gmial.com>
2025-01-10 17:02:37 +0900

    課題０２０の再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,16 +1,5 @@
-const addBtn = document.getElementById('btn');
-
-const parentList = document.getElementById('text');
-
-addBtn.addEventListener('click', () => {
-
-  // li要素を新しく作成する
-  const childList = document.createElement('text');
-  childList.textContent = 'ボタンをクリックしました';
-
-  // parentListの最初の子要素を削除する
-  parentList.removeChild(parentList.firstChild);
-
-  // childListを追加する
-  parentList.appendChild(childList);
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました。';
 });
